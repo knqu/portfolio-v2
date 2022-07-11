98a7bcf2e8865d9dcb1f610c61cd7981b7d0d826
Kevin <kevinqu2542@gmail.com>
2022-07-11 17:06:12 -0400

    feat: implement preloader css animation
    Add gsap animations to transition from preloader to main content.

--- public/js/script.js
+++ public/js/script.js
@@ -4,30 +4,41 @@ const about = document.querySelector('#about');
 const works = document.querySelector('#works');
 const contact = document.querySelector('#contact');
 
-// gsap animations
+// gsap animations and preloader
 // gsap-related css set in javascript so website will be unaffected if js were to be disabled
+
+const preloader = document.querySelector('#preloader');
+const preloaderWrapper = document.querySelector('#preloaderWrapper');
+
+const main = document.querySelector('main');
+main.style.opacity = '0';
 
 const landingText = document.querySelector('#landingText');
 landingText.style.clipPath = 'polygon(0 100%, 100% 100%, 100% 100%, 0% 100%)';
-landingText.style.opacity = '0';
 landingText.style.transform = 'translateY(25%)';
 
-const mainNav = document.querySelector('#mainNav');
-mainNav.style.opacity = '0';
+// all gsap utilities must be executed after page has loaded due to scripts being deferred
+window.addEventListener('load', function () {
+    gsap.registerPlugin(ScrollTrigger);
 
-gsap.registerPlugin(ScrollTrigger);
+    const tl = gsap.timeline({ defaults: { duration: 2 } });
+    tl
+        .to(preloader, {
+            opacity: 0
+        })
+        .to(main, {
+            opacity: 1
+        })
+        .to(landingText, {
+            'clip-path': 'polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)',
+            y: 0,
+            ease: 'back'
+        }, '<');
 
-gsap.to('#landingText', {
-    'clip-path': 'polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)',
-    opacity: 1,
-    y: 0,
-    duration: 2,
-    ease: 'back'
-});
-
-gsap.to('#mainNav', {
-    opacity: 1,
-    duration: 2
+    setTimeout(function () {
+        preloader.remove();
+        preloaderWrapper.remove();
+    }, 2000);
 });
 
 // links and buttons: uses js instead of <a> to circumvent url change
