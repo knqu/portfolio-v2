93614da3e42155a6b81f6812a1b7687337a71c10
Kevin <kevinqu2542@gmail.com>
2022-07-27 21:04:05 -0400

    update: make dotenv dependency from dev dependency

--- controllers/notify.js
+++ controllers/notify.js
@@ -1,7 +1,4 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
-}
-
+require('dotenv').config();
 const nodemailer = require('nodemailer');
 
 const adminEmail = process.env.EMAIL_ADDRESS;
